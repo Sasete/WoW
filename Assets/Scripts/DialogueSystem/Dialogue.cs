33cdc3dc2e0013e89b62ce6bec9c797bd67ae930
Sasete <44236927+Sasete@users.noreply.github.com>
2022-03-14 22:42:56 +0300

    Dialogue System
    Dialogue System and example Dialogue prepared.

---
+++ Assets/Scripts/DialogueSystem/Dialogue.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue")]
+public class Dialogue : ScriptableObject
+{
+
+    public string talker;
+
+    public string text;
+
+
+    public UnityEvent onAccept;
+    public UnityEvent onReject;
+
+
+    public Dialogue()
+    {
+        onAccept = new UnityEvent();
+        onReject = new UnityEvent();
+    }
+
+}
