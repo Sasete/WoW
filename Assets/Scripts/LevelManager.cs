695d7356e37534d9255e6727d558380795be0356
Nihat <basatemurnihat08@gmail.com>
2022-03-14 19:35:10 +0300

    level manager improved

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,12 +5,42 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
 
+    public static LevelManager level;
+
+    public float time = 0;
+
+    public void Start()
+    {
+
+        if(level != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        level = this;
+
+    }
+
+    public void Update()
+    {
+
+        time += Time.deltaTime;
+
+    }
+
+    public void OnDestroy()
+    {
+
+        if(level == this)
+            level = null;
+
+    }
+
 
     public void NextLevel()
     {
         Player.instance.NextLevel();
     }
-
-
 
 }
